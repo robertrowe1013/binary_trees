2088094dd34a16b52e56465e471e4fef202cdac4
robertrowe1013 <robertrowe1013@gmail.com>
2020-07-20 18:52:53 +0000

    task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+  * binary_tree_delete - delete tree
+  * @tree: tree
+  */
+void binary_tree_delete(binary_tree_t *tree)
+{
+    binary_tree_t *left_branch;
+    binary_tree_t *right_branch;
+    if (tree == NULL)
+        return;
+    left_branch = tree->left;
+    right_branch = tree->right;
+    if (left_branch != NULL)
+        binary_tree_delete(left_branch);
+    if (right_branch != NULL)
+        binary_tree_delete(right_branch);
+    free(tree);
+}
