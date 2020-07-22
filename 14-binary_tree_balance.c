bb60a7d93276ea815018ff8cc6cf5eeaf6941ec6
robertrowe1013 <robertrowe1013@gmail.com>
2020-07-22 18:16:07 +0000

    corrected 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,9 +14,31 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->left)
-		left_height = binary_tree_balance(tree->left) + 1;
+		left_height = binary_tree_height(tree->left) + 1;
 	if (tree->right)
-		right_height = binary_tree_balance(tree->right) + 1;
+		right_height = binary_tree_height(tree->right) + 1;
 	balance = left_height - right_height;
 	return (balance);
+}
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: pointer to the root node of the tree to measure the height.
+ * Recursively traverse each subtree, adding to LeftHeighT or RightHeighT
+ * total. Then, compare values.
+ * Return: Height (aka, tallest subtree)
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t lht, rht;
+
+	lht = rht = 0;
+	if (tree == NULL)
+		return (0);
+	if (tree->left)
+		lht = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		rht = binary_tree_height(tree->right) + 1;
+	if (lht > rht)
+		return (lht);
+	return (rht);
 }
